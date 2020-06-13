f4da968fb19c132b22a09772b9fbdff827eb66af
ajboni <mail@aboni.dev>
2020-06-12 22:03:25 -0300

    Basic mod-host container

--- mod-host/run.sh
+++ mod-host/run.sh
@@ -3,10 +3,10 @@
 # Run the container
 # Need to add the audio group, the current user groups, add limits for RT kernel and map /dev/shm so we can connect to JACK
 
-docker run --rm \
+docker run \
+--rm -it \
 --group-add audio \
 --group-add $(id -g) \
--it \
 --volume=/dev/shm:/dev/shm:rw \
 --user=$(id -u):$(id -g) \
 --ulimit rtprio=99 \
