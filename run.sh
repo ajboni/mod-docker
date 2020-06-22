37f9e16a69db9089abb1c1f30280bd7ee5a32ac3
ajboni <baj@mailbox.org>
2020-06-22 19:47:23 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 export GROUP_NAME=$(id -gn)
 
+
+docker build  --rm -f "mod-host/Dockerfile" -t mod-host "mod-host"
 docker build  --rm -f "mod-ui/Dockerfile" -t mod-ui "mod-ui"
 docker-compose up  --no-build mod-ui
